7d06553f5b508e817c99eed3dd26f478f4364011
qoheniac <29815186+qoheniac@users.noreply.github.com>
2020-12-14 23:13:42 +0100

    Formatting Day 12 Part 2 (Clojure)

--- 12/part2.clj
+++ 12/part2.clj
@@ -1,40 +1,37 @@
 (require '[clojure.string :as string :refer [split-lines]])
 
-(defn abs [x]
-  (if (> x 0) x (- x)))
-
 (defn read-data [f]
   (map #(let [match (next (re-matches #"([A-Z])(\d+)" %))]
-          (list (first match) (Integer. (second match))))
+          [(first match) (read-string (second match))])
        (split-lines (slurp f))))
 
 (defn move [loc dir vel]
-  (list
-    (+ (first loc) (* vel (first dir)))
-    (+ (second loc) (* vel (second dir)))))
+  [(+ (first loc) (* vel (first dir)))
+   (+ (second loc) (* vel (second dir)))])
 
 (defn rotate [wploc angle]
   (case angle
-    90 (list (second wploc) (- (first wploc)))
+    90 [(second wploc) (- (first wploc))]
     180 (map - wploc)
-    270 (list (- (second wploc)) (first wploc))))
+    270 [(- (second wploc)) (first wploc)]))
 
 (defn exec [loc wploc cmd]
-  (let [action (first cmd) number (second cmd)]
-    (if (= action "F")
-      (list (move loc wploc number) wploc)
-      (if (= action "R")
-        (list loc (rotate wploc number))
-        (if (= action "L")
-          (list loc (rotate wploc (- 360 number)))
-          (list loc (case action
-                      "N" (move wploc (list 0 1) number)
-                      "E" (move wploc (list 1 0) number)
-                      "S" (move wploc (list 0 -1) number)
-                      "W" (move wploc (list -1 0) number))))))))
+  (let [action (first cmd)
+        number (second cmd)]
+    (case action
+      "F" {:loc (move loc wploc number) :wploc wploc}
+      "R" {:loc loc :wploc (rotate wploc number)}
+      "L" {:loc loc :wploc (rotate wploc (- 360 number))}
+          {:loc loc :wploc (case action
+                 "N" (move wploc [0 1] number)
+                 "E" (move wploc [1 0] number)
+                 "S" (move wploc [0 -1] number)
+                 "W" (move wploc [-1 0] number))})))
 
-(loop [loc (list 0 0) wploc (list 10 1) cmds (read-data "input")]
+(loop [loc [0 0]
+       wploc [10 1]
+       cmds (read-data "input")]
   (if (nil? cmds)
-    (println (apply + (map abs loc)))
+    (println (apply + (map #(Math/abs %) loc)))
     (let [locwploc (exec loc wploc (first cmds))]
-      (recur (first locwploc) (second locwploc) (next cmds)))))
+      (recur (:loc locwploc) (:wploc locwploc) (next cmds)))))
