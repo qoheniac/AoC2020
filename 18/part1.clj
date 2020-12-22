a8d4b5d269b6b31f306512bc5f83df11512d4658
qoheniac <29815186+qoheniac@users.noreply.github.com>
2020-12-22 12:28:29 +0100

    remove empty lines inside function and sort arguments

--- 18/part1.clj
+++ 18/part1.clj
@@ -1,30 +1,28 @@
 (require '[clojure.string :refer [split-lines]])
 
 (defn calc
-
   ([expression]
    (let [sym (first expression)
          todo (next expression)]
      (if (= sym \))
        (apply calc (calc todo))
-       (calc (Character/digit sym 10) todo))))
-  
-  ([number expression]
+       (calc todo (Character/digit sym 10)))))
+  ([expression number]
    (let [todo (next expression)]
      (case (first expression)
-       nil [number nil]
-       \( [number todo]
-       (let [[number2 todo2] (calc todo)]
+       nil [nil number]
+       \( [todo number]
+       (let [[todo2 number2] (calc todo)]
          (case (first expression)
-           \+ [(+ number number2) todo2]
-           \* [(* number number2) todo2]))))))
+           \+ [todo2 (+ number number2)]
+           \* [todo2 (* number number2)]))))))
 
 (defn parse-line [line]
   (->> line
        reverse
        (remove #(= % \ ))
        calc
-       first))
+       second))
 
 (->> "input"
      slurp
